6b36f61fd478486a31d3728eae64960b69b017d0
Josa Gesell <josa@gesell.me>
2021-12-18 21:04:06 +0100

    ci: Add StyLua workflow

--- lua/jg/project-config.lua
+++ lua/jg/project-config.lua
@@ -1,4 +1,4 @@
-local configHome = os.getenv("XDG_CONFIG_HOME") or os.getenv("HOME") .. '/.config'
+local configHome = os.getenv('XDG_CONFIG_HOME') or os.getenv('HOME') .. '/.config'
 local configDir = configHome .. '/nvim'
 
 local M = {}
@@ -8,7 +8,7 @@ M.loaded = false
 local separator = '----'
 
 function M.global_config(dir)
-  return configDir .. '/projects/' .. dir:gsub('^' .. os.getenv("HOME") .. '/', ""):gsub('/+$', ''):gsub('/', separator)
+  return configDir .. '/projects/' .. dir:gsub('^' .. os.getenv('HOME') .. '/', ''):gsub('/+$', ''):gsub('/', separator)
 end
 
 function M.local_config(dir)
@@ -16,17 +16,19 @@ function M.local_config(dir)
 end
 
 function M.find_config(gen)
-  local pattern = "%:p"
+  local pattern = '%:p'
 
   while true do
-    pattern = pattern .. ":h"
+    pattern = pattern .. ':h'
     local dir = vim.fn.expand(pattern)
 
-    if dir == "/" then return end
+    if dir == '/' then
+      return
+    end
 
-    for _,conf_dir_fn in pairs(gen) do
+    for _, conf_dir_fn in pairs(gen) do
       local conf_dir = conf_dir_fn(dir)
-      for _,ext in pairs({ '.lua', '.vim' }) do
+      for _, ext in pairs({ '.lua', '.vim' }) do
         if vim.fn.filereadable(conf_dir .. '/init' .. ext) ~= 0 then
           return conf_dir .. '/init' .. ext
         end
@@ -35,9 +37,10 @@ function M.find_config(gen)
   end
 end
 
-
 function M.load_config(force)
-  if M.loaded and not force then return end
+  if M.loaded and not force then
+    return
+  end
 
   local config = M.find_config({ M.local_config, M.global_config })
   if config then
@@ -47,13 +50,12 @@ function M.load_config(force)
 end
 
 function M.setup()
-  vim.cmd [[
+  vim.cmd([[
     augroup 'jg.project-config'
       autocmd!
       autocmd VimEnter * lua require('jg.project-config').load_config()
     augroup END
-  ]]
+  ]])
 end
 
 return M
-
